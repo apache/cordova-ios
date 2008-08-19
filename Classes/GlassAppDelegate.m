09f17ac40b3b98e6236842ffd6c9f18647b51201
Rob Ellis <kazoomer@gmail.com>
2008-08-18 22:33:00 -0700

    Working on Photo Upload - Updated with new JavaScript API

--- Classes/GlassAppDelegate.m
+++ Classes/GlassAppDelegate.m
@@ -3,6 +3,11 @@
 #import "GlassViewController.h"
 #import <AudioToolbox/AudioServices.h>
 #import <UIKit/UIDevice.h>
+#import <UIKit/UINavigationController.h>
+#import <UIKit/UIKit.h>
+
+#import <CoreLocation/CoreLocation.h>
+
 
 @implementation GlassAppDelegate
 
@@ -22,9 +27,14 @@
 	
 	// Set up the image picker controller and add it to the view
 	imagePickerController = [[UIImagePickerController alloc] init];
-
+	
+	
+	// Im not sure why the next line was giving me a warning... any ideas?
+	// when this is commented out, the cancel button no longer works.
+	imagePickerController.delegate = self;
 	imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    [window addSubview:imagePickerController.view];
+	[window addSubview:imagePickerController.view];
+
 	
 	[[UIAccelerometer sharedAccelerometer] setUpdateInterval:1.0/40.0];
 	[[UIAccelerometer sharedAccelerometer] setDelegate:self];
@@ -45,11 +55,14 @@
 	NSBundle * thisBundle = [NSBundle bundleForClass:[self class]];
 	
 	/*
+	 
+	 // OFF-LINE related code. (Still in testing.
 	if (urlPathString = [thisBundle pathForResource:htmlFileName	ofType:@"html"]){
 		NSString * test = [NSString stringWithContentsOfFile:urlPathString];
 		//NSString * test;
 		//test = @"<h1>Hello World</h1>";
 
+		test = [webView stringByEvaluatingJavaScriptFromString:test];
 		[webView loadHTMLString:test baseURL:nil];
 	 
 	}
@@ -106,6 +119,7 @@
 				NSLog(@"location request!");
 
 				jsCallBack = [[NSString alloc] initWithFormat:@"gotLocation('%f','%f');", lat, lon];
+				NSLog(jsCallBack);
 				[webView2 stringByEvaluatingJavaScriptFromString:jsCallBack];
 				
 				[jsCallBack release];
@@ -117,29 +131,19 @@
 				[jsCallBack release];
 			}
             else if([(NSString *)[parts objectAtIndex:1] isEqualToString:@"getphoto"]){
-				NSLog(@"photo request!");
+				NSLog(@"Photo request!");
 				NSLog([parts objectAtIndex:2]);
                 
-                // webView.hidden = YES;
+				webView.hidden = YES;
                 [window bringSubviewToFront:imagePickerController.view];
 				NSLog(@"photo dialog open now!");
             }
-			
 			
 			//VIBRATION
 			else if([(NSString *)[parts objectAtIndex:1] isEqualToString:@"vibrate"]){
 				NSLog(@"vibration request!");
 				AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
 			}
-			
-			//PHOTO-PICKER
-			else if([(NSString *)[parts objectAtIndex:1] isEqualToString:@"getphoto"]){
-				NSLog(@"photo request!");
-                
-                // webView.hidden = YES;
-                [window bringSubviewToFront:imagePickerController.view];
-				NSLog(@"photo dialog open now!");
-            }
 			
 			return NO;
 		}
@@ -151,26 +155,17 @@
 
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
+	 
 	[lastKnownLocation release];
 	lastKnownLocation = newLocation;
 	[lastKnownLocation retain];
 	printf("Updating Location to : %s",[[lastKnownLocation description] UTF8String]);  
 	
-	double lat = lastKnownLocation.coordinate.latitude;
-	double lng = lastKnownLocation.coordinate.longitude;
-	
 	passPersonalInfo = YES;
 	passGeoData = YES;
 	
-	//This is how you do it with a GET
-	NSString *urlTemp = nil;
-	//NSString *personalInfoTemp = @"?personalInfo=BrockWhitten";
-	//NSString *geoDataTemp = [[NSString alloc] initWithFormat:@"&geoData=%@", @"foo" ];
-	NSString *latTemp = [[NSString alloc] initWithFormat:@"lat=%f", lat ];
-	NSString *lngTemp = [[NSString alloc] initWithFormat:@"&long=%f", lng ];
-	urlTemp = [[NSString alloc] initWithFormat:@"http://clayburn.org/iphone.php?%@%@", latTemp, lngTemp];
-	
-	//[[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlTemp]];
+	double lat = lastKnownLocation.coordinate.latitude;
+	double lng = lastKnownLocation.coordinate.longitude;
 	
 }
 
@@ -185,54 +180,104 @@
 	NSLog(ret);
 }
 
-
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image editingInfo:(NSDictionary *)editingInfo
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingImage:(UIImage *)image2 editingInfo:(NSDictionary *)editingInfo
 {
+	
+	[picker dismissModalViewControllerAnimated:YES];
+    imagePickerController.view.hidden = YES;
+	NSLog(@"Photo Picked");
+	NSData * imageData = UIImageJPEGRepresentation(image2, 75);
+	
+//	NSURLRequest * systemPost = [self sendPhotoToCallback:imageData];
     
 	// Dismiss the image selection, hide the picker and show the image view with the picked image
-	[picker dismissModalViewControllerAnimated:YES];
-	imagePickerController.view.hidden = YES;
-	
-  	UIDevice * dev = [UIDevice currentDevice];
-	NSString *uniqueId = dev.uniqueIdentifier;
-	
-	//UIImage *testImage = [UIImage imageNamed:@"image1.png"];
-	
-	
-    NSData * imageData = UIImageJPEGRepresentation(image, 75);
-	
-	//NSData * imageData = UIImagePNGRepresentation(image);
-	
-	NSString *postLength = [NSString stringWithFormat:@"%d", [imageData length]];
-	
-	NSString *urlString = [@"http://phonegap.com/demo/upload.php?" stringByAppendingString:@"ve=iPhoneV0&mcid"];
-	urlString = [urlString stringByAppendingString:uniqueId];
-	urlString = [urlString stringByAppendingString:@"&lang=en_US.UTF-8"];
-	
-	NSMutableURLRequest *request = [[[NSMutableURLRequest alloc] init] autorelease];
-	[request setURL:[NSURL URLWithString:urlString]];
-	[request setHTTPMethod:@"POST"];
-	[request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-	[request setHTTPBody:imageData];
-	
-	NSURLConnection *conn=[[NSURLConnection alloc] initWithRequest:request delegate:self];
-	if (conn)
-	{
-		NSLog(@"Sucess");	
-		//receivedData = [[NSMutableData data] retain];
-	}   
+	//[picker dismissModalViewControllerAnimated:YES];
+	//imagePickerController.view.hidden = YES;
 
 	webView.hidden = NO;
 	[window bringSubviewToFront:webView];
 }
 
+/*
+- (BOOL)sendPhotoToCallback:(NSData *)imageData {
+	NSLog(@"IN SEND");
+	
+	if (!imageData) return NO;
+	
+	
+	//Create dictionary of post arguments
+	NSArray *keys = [[NSArray alloc] initWithObjects:@"email"];
+	NSArray *objects = [[NSArray alloc] initWithObjects:
+						[NSString stringWithFormat:@"%@",CFPreferencesCopyAppValue(CFSTR("TumblrEmail"), kCFPreferencesCurrentApplication)]];
+	NSDictionary *keysDict = [[NSDictionary alloc] initWithObjects:objects forKeys:keys];
+	
+	NSURLRequest *callBackPost = [self createCallBackRequest:keysDict withData:imageData];
+	
+	//send request, return YES if successful
+	callBackConnection = [[NSURLConnection alloc] initWithRequest:callBackPost delegate:self];
+	if (!callBackConnection) {
+		NSLog(@"Failed to submit request");
+		return NO;
+	} else {
+		NSLog(@"Request submitted");
+		NSMutableData *receivedData = [[NSMutableData data] retain];
+		return YES;
+	}
+}
+
+
+-(NSURLRequest *)createCallBackRequest:(NSDictionary *)postKeys withData:(NSData *)data {
+	NSLog(@"IN Create Request");
+	
+	//create the URL POST Request to tumblr
+	NSURL *callbackURL = [NSURL URLWithString:@"http://phonegap.com/demo/upload.php"];
+	NSMutableURLRequest *callbackPost = [NSMutableURLRequest requestWithURL:callbackURL];
+	[callbackPost setHTTPMethod:@"POST"];
+	
+	//Add the header info
+	NSString *stringBoundary = [NSString stringWithString:@"0xKhTmLbOuNdArY"];
+	NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",stringBoundary];
+	[callbackPost addValue:contentType forHTTPHeaderField: @"Content-Type"];
+	
+	//create the body
+	NSMutableData *postBody = [NSMutableData data];
+	[postBody appendData:[[NSString stringWithFormat:@"--%@\r\n",stringBoundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	
+	//add key values from the NSDictionary object
+	NSEnumerator *keys = [postKeys keyEnumerator];
+	int i;
+	for (i = 0; i < [postKeys count]; i++) {
+		NSString *tempKey = [keys nextObject];
+		[postBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n",tempKey] dataUsingEncoding:NSUTF8StringEncoding]];
+		[postBody appendData:[[NSString stringWithFormat:@"%@",[postKeys objectForKey:tempKey]] dataUsingEncoding:NSUTF8StringEncoding]];
+		[postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",stringBoundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	}
+	
+	//add data field and file data
+	[postBody appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"data\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+	[postBody appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+	[postBody appendData:[NSData dataWithData:data]];
+	[postBody appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",stringBoundary] dataUsingEncoding:NSUTF8StringEncoding]];
+	
+	//add the body to the post
+	[callbackPost setHTTPBody:postBody];
+	
+	return callbackPost;
+		
+}
+*/
  
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
 {
 	// Dismiss the image selection and close the program
-	[picker dismissModalViewControllerAnimated:YES];
-	image = nil;
-}
+    [picker dismissModalViewControllerAnimated:YES];
+	// Hide the imagePicker and bring the web page back into focus
+    imagePickerController.view.hidden = YES;
+	NSLog(@"Photo Cancel Request");
+	webView.hidden = NO;
+	[window bringSubviewToFront:webView];
+	
+	}
 
 - (void)dealloc {
     [viewController release];
