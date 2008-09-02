c07a7305c0858d4463f3694221743f14ef65073d
Brock Whitten <brock@sintaxi.com>
2008-09-02 15:14:39 -0700

    added in UUID - Thanks Brig

--- Classes/Device.m
+++ Classes/Device.m
@@ -12,9 +12,11 @@
 				  __gap = true; \
 				  __gap_version='0.1'; \
 				  __gap_device_model='%s'; \
-				  __gap_device_version='%s';",
+				  __gap_device_version='%s';\
+				  __gap_device_uniqueid='%s';",
 				  [[myCurrentDevice model] UTF8String],
-				  [[myCurrentDevice systemVersion] UTF8String]
+				  [[myCurrentDevice systemVersion] UTF8String],
+				  [[myCurrentDevice uniqueIdentifier] UTF8String]
 				  ];
 	//NSLog(jsCallBack);
 }
