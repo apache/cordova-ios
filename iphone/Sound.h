71db8c62974868d59882d5c256da2a0f1ddab263
Rob Ellis <kazoomer@gmail.com>
2008-12-16 00:58:24 -0800

    Renamed SoundEffect to Sound

---
+++ iphone/Sound.h
@@ -0,0 +1,19 @@
+//
+//  sound.h
+//  Glass
+//
+//  Created by Nitobi on 12/12/08.
+//  Copyright 2008 Nitobi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioServices.h>
+
+@interface Sound : NSObject {
+	SystemSoundID soundID;
+}
+
+- (id)initWithContentsOfFile:(NSString * )path;
+- (void)play;
+
+@end
---
+++ iphone/Sound.m
@@ -0,0 +1,31 @@
+//
+//  sound.m
+//  Glass
+//
+//  Created by Nitobi on 12/12/08.
+//  Copyright 2008 Nitobi. All rights reserved.
+//
+
+#import "Sound.h"
+
+
+@implementation Sound
+
+- (id) initWithContentsOfFile:(NSString *)path
+{
+	self = [super init];
+	if (self != nil) {
+		NSURL *filePath = [NSURL fileURLWithPath:path isDirectory:NO];
+		AudioServicesCreateSystemSoundID((CFURLRef)filePath, &soundID);
+	}
+	return self;
+}
+
+- (void) play {
+	AudioServicesPlaySystemSound(soundID);
+}
+
+- (void) dealloc {
+	[super dealloc];
+}
+@end
