2d259789bdbe8807e827ae231b87470eb68c4302
Michael Nachbaur <mike@nachbaur.com>
2009-05-06 17:33:20 -0700

    Extended konobi's build-time XCode script to build JavaScript sources. Added a "configure" script to generate a Makefile which builds the necessary JavaScript libraries in a clean fashion, keeping in mind dependancy change times. So therefore it will rebuild only the files needed when a "make" is run. Makes documentation as well, now in "docs/javascript".
    I've also removed the pre-built PhoneGap JS libraries and documentation, as
    generated files shouldn't be checked in.  It made merging between branches
    extremely difficult.
    
    I've left the Rakefile, but I'd like to see the Makefile used, since it is
    more flexible, and respects file timestamps.

--- iphone/build-phonegap.sh
+++ iphone/build-phonegap.sh
@@ -1,1 +1,9 @@
-echo "Building PhoneGap"
+#!/bin/sh
+set -x
+PHONEGAP_LIB=$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/www/phonegap.js
+[ -f $PHONEGAP_LIB ] && exit 0
+
+cd $PROJECT_DIR/..
+[ -f Makefile ] || ./configure
+make iphone
+cp lib/iphone/phonegap-min.js $PHONEGAP_LIB
