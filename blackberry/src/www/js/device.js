a5928034c03c3d45b293b74beecdbe90cdb8ce97
xmaniac <jose.noheda@gmail.com>
2009-05-18 11:23:49 +0200

    Ported Device and Vibration API

--- blackberry/src/www/js/device.js
+++ blackberry/src/www/js/device.js
@@ -1,47 +1,33 @@
-function addOnLoad(f) {
-	if (window.onload) {
-		var previousOnload = window.onload; 
-		window.onload = function() {
-			previousOnload.apply(window);
-			f.apply(window);
-		}
-	} else window.onload = f;	
-}
+window.Device = {
+    isIPhone: false,
+    isIPod: false,
+    isBlackBerry: true,
 
-addOnLoad(function() {
-	window.Device = {
-	    isIPhone: false,
-	    isIPod: false,
-	    isBlackBerry: true,
+    init: function() {
+		this.exec("initialize");
+		this.poll(function() {
+			Device.available = typeof Device.model == "string";
+		});
+    },
+    exec: function(command, params, sync) {
+        if (Device.available || command == "initialize") {
+            try {
+                var url = "gap://" + command;
+                if (params) url += "/" + params.join("/");
+                document.location = url;
+                if (sync) this.poll();
+            } catch(e) {
+                console.log("Command '" + command + "' has not been executed, because of exception: " + e);
+                alert("Error executing command '" + command + "'.")
+            }
+        }
+    },
+    poll: function(callback) {
+    	eval(document.cookie + (callback ? ";callback();" : ""));
+    },
+    vibrate: function(secs) {
+        return Device.exec("vibrate", [secs]);
+    }
+};
 
-	    init: function() {
-			this.exec("initialize");
-			this.poll(function() {
-				Device.available = typeof Device.model == "string";
-			});
-	    },
-	    exec: function(command, params, sync) {
-	        if (Device.available || command == "initialize") {
-	            try {
-	                var url = "gap://" + command;
-	                if (params) url += "/" + params.join("/");
-	                document.location = url;
-	                if (sync) this.poll();
-	            } catch(e) {
-	                console.log("Command '" + command + "' has not been executed, because of exception: " + e);
-	                alert("Error executing command '" + command + "'.")
-	            }
-	        }
-	    },
-	    poll: function(callback) {
-	    	eval(document.cookie + (callback ? ";callback();" : ""));
-	    },
-	    vibrate: function(secs) {
-	        return Device.exec("vibrate", [secs]);
-	    }
-	};
-});
-
-addOnLoad(function() {
-	window.Device.init();
-});
+window.Device.init();
