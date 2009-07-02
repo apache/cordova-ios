38d370ad152f3d249f9ec407ca8525598e61bc85
David Orchard <davidorchard@david-orchards-macbook-pro.local>
2009-07-02 12:00:52 -0700

    Added quotes to $phonegap_lib to allow spaces in project directories

--- iphone/build-phonegap.sh
+++ iphone/build-phonegap.sh
@@ -5,4 +5,4 @@ PHONEGAP_LIB=$TARGET_BUILD_DIR/$CONTENTS_FOLDER_PATH/www/phonegap.js
 cd $PROJECT_DIR/..
 [ -f Makefile ] || ./configure
 make iphone
-cp lib/iphone/phonegap-min.js $PHONEGAP_LIB
+cp lib/iphone/phonegap-min.js "$PHONEGAP_LIB"
