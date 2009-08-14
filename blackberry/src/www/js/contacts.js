b0da6167e1b60f2ee60bdfa344d0d2fcb61a1779
Fil Maj <filip.maj@nitobi.com>
2009-08-13 18:28:19 -0700

    Updated Contacts to work with PhoneGap BlackBerry. Following Shazrons lead on the JS API for the contacts with respect to parameters.

--- blackberry/src/www/js/contacts.js
+++ blackberry/src/www/js/contacts.js
@@ -6,26 +6,45 @@ function Contact() {
 	this.name = "";
 	this.phone = "";
 	this.address = "";
+	this.email = "";
 }
-
-/**
- * 
- * @param {Object} successCallback
- * @param {Object} errorCallback
- * @param {Object} options
- */
-Contact.prototype.get = function(successCallback, errorCallback, options) {
-	
-}
-
 
 function ContactManager() {
 	this.contacts = [];
-	this.timestap = new Date().getTime();
+	this.timestamp = new Date().getTime();
+	// Options used when calling ContactManager functions.
+	this.options = {
+		'pageSize':0,
+		'pageNumber':0,
+		'nameFilter':''
+	};
 }
-
-ContactManager.prototype.get = function(successCallback, errorCallback, options) {
-	Device.exec("contacts", [options.operation, options.field, options.value], true);
+ContactManager.prototype.formParams = function(options, startArray) {
+	var params = [];
+	if (startArray) params = startArray;
+	if (options.pageSize && options.pageSize > 0) params.push("pageSize:" + options.pageSize);
+	if (options.pageNumber) params.push("pageNumber:" + options.pageNumber);
+	if (options.nameFilter) params.push("nameFilter:" + options.nameFilter);
+	return params;	
+}
+ContactManager.prototype.displayContact = function(successCallback, errorCallback, options) {
+	if (options.nameFilter && options.nameFilter.length > 0) {
+		var params = ["search"];
+		params = this.formParams(options,params);
+		this.search_onSuccess = successCallback;
+		this.search_onError = errorCallback;
+		device.exec("contacts", params, true);
+	} else {
+		ContactManager.getAllContacts(successCallback,errorCallback,options);
+		return;
+	}
+}
+ContactManager.prototype.getAllContacts = function(successCallback, errorCallback, options) {
+	this.global_onSuccess = successCallback;
+	this.global_onError = errorCallback;
+	var params = ["getall"];
+	params = this.formParams(options,params);
+	device.exec("contacts", params, true);
 }
 
 if (typeof navigator.ContactManager == "undefined") navigator.ContactManager = new ContactManager();
