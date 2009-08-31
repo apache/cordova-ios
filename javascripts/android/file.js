6c963fae5cca6cba894954228039400321663e2f
Joe Bowser <bowserj@volti.(none)>
2009-08-31 11:32:24 -0700

    Hacking on the File and Media APIs

---
+++ javascripts/android/file.js
@@ -0,0 +1,27 @@
+File.prototype.read = function(fileName, successCallback, errorCallback) {
+  var data = navigator.FileUtil.read(fileName);
+  if( data.substr("FAIL"))
+  {
+    errorCallback(); 
+  }
+  else
+  {
+    successCallback(data);
+  }
+}
+
+/**
+ * Writes a file to the mobile device.
+ * @param {File} file The file to write to the device.
+ */
+File.prototype.write = function(file, str, successCallback, failCallback) {
+	var call = FileUtil.write(str);
+  if(call == 0)
+  {
+    successCallback();
+  }
+  else
+  {
+    failCallback(call);
+  }
+}
