7a11eb72f575860857fcc459db70f2bbbbde5f69
Fil Maj <filip.maj@nitobi.com>
2009-10-14 20:39:14 -0700

    Cleaned up Contacts, Device and GeoLocation API functionality implementation classes, and updated the proper device javascript file.

--- blackberry/src/com/nitobi/phonegap/api/impl/DeviceCommand.java
+++ blackberry/src/com/nitobi/phonegap/api/impl/DeviceCommand.java
@@ -34,6 +34,17 @@ import net.rim.device.api.system.DeviceInfo;
 public class DeviceCommand implements Command {
 
 	private static final String CODE = "PhoneGap=initialize";
+	private static final String EMULATOR = "Emulator";
+	private static final String DEVICE_NAME = ";device.name = '";
+	private static final String DEVICE_FLASH = "';device.flash = ";
+	private static final String DEVICE_PLATFORM = ";device.platform = '";
+	private static final String DEVICE_VENDOR = "';device.vendor = '";
+	private static final String DEVICE_BATTERY = "';device.battery = ";
+	private static final String DEVICE_VERSION = ";device.version = '";
+	private static final String DEVICE_SIMULATOR = "';device.isSimulator = ";
+	private static final String DEVICE_CAMERA = ";device.hasCamera = ";
+	private static final String DEVICE_UUID = ";device.uuid = ";
+	private static final String SEMI_COLON = ";device.uuid = ";
 
 	/**
 	 * Determines whether the specified instruction is accepted by the command. 
@@ -58,16 +69,16 @@ public class DeviceCommand implements Command {
 	 * 
 	 */
 	public String execute(String instruction) {
-		StringBuffer deviceInfo = new StringBuffer(";device.name = '");
-		deviceInfo.append(DeviceInfo.getDeviceName()).append("';device.flash = ");
-		deviceInfo.append(DeviceInfo.getTotalFlashSize()).append(";device.platform = '");
-		deviceInfo.append(DeviceInfo.getPlatformVersion().length()>0?DeviceInfo.getPlatformVersion():"Emulator").append("';device.vendor = '");
-		deviceInfo.append(DeviceInfo.getManufacturerName()).append("';device.battery = ");
-		deviceInfo.append(DeviceInfo.getBatteryLevel()).append(";device.version = '");
-		deviceInfo.append(DeviceInfo.getSoftwareVersion()).append("';device.isSimulator = ");
-		deviceInfo.append(DeviceInfo.isSimulator()).append(";device.hasCamera = ");
-		deviceInfo.append(DeviceInfo.hasCamera()).append(";device.uuid = ");
-		deviceInfo.append(DeviceInfo.getDeviceId()).append(";");
+		StringBuffer deviceInfo = new StringBuffer(DEVICE_NAME);
+		deviceInfo.append(DeviceInfo.getDeviceName()).append(DEVICE_FLASH);
+		deviceInfo.append(DeviceInfo.getTotalFlashSize()).append(DEVICE_PLATFORM);
+		deviceInfo.append(DeviceInfo.getPlatformVersion().length()>0?DeviceInfo.getPlatformVersion():EMULATOR).append(DEVICE_VENDOR);
+		deviceInfo.append(DeviceInfo.getManufacturerName()).append(DEVICE_BATTERY);
+		deviceInfo.append(DeviceInfo.getBatteryLevel()).append(DEVICE_VERSION);
+		deviceInfo.append(DeviceInfo.getSoftwareVersion()).append(DEVICE_SIMULATOR);
+		deviceInfo.append(DeviceInfo.isSimulator()).append(DEVICE_CAMERA);
+		deviceInfo.append(DeviceInfo.hasCamera()).append(DEVICE_UUID);
+		deviceInfo.append(DeviceInfo.getDeviceId()).append(SEMI_COLON);
 		return deviceInfo.toString();
 	}
 
