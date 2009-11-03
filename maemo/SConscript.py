88164dc17e2f79230ae932c2ba84449ecc463378
Jussi Toivola <jussi@jussi-laptop-vr.(none)>
2009-11-03 15:02:53 +0200

    Initial maemo support using scheme to map commands

---
+++ maemo/SConscript.py
@@ -0,0 +1,48 @@
+import os
+from os.path import join, exists
+
+PHONEGAP_LIB        = "phonegap.js"
+PHONEGAP_COMPRESSED = "app.js"
+APPNAME = "PhoneGap"
+
+def CommandMakePhonegapJS(env, source, target):
+    
+    print target[0].path
+    ft = open( target[0].path, 'wb' )
+    
+    for s in source:
+        fs = open( s.path, 'rb')
+        d = d = fs.read();
+        while len(d) != 0:
+            ft.write( d )
+            d = fs.read();
+            
+        fs.close()        
+        
+        ft.write( os.linesep )
+        
+    ft.close()
+ 
+jssources = [
+    "phonegap.base",
+    "phonegap",
+]
+
+javascripts = []
+JSROOTPATH= "../javascripts"
+for js in jssources:
+    baselib = join( JSROOTPATH, "%s.js" % js )
+    if exists( baselib ): 
+        javascripts.append( baselib )    
+    
+    iphonelib = join( JSROOTPATH, "maemo/%s.js" % js )
+    if exists( iphonelib ): 
+        #print iphonelib
+        javascripts.append( iphonelib )
+    
+Command( PHONEGAP_LIB, javascripts, CommandMakePhonegapJS )
+
+# Compress javascripts
+compress = "java -jar ../util/yuicompressor-2.4.2.jar --charset UTF-8 -o $TARGET $SOURCE"
+Command( PHONEGAP_COMPRESSED, PHONEGAP_LIB, compress )
+
