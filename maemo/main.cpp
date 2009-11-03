47affb42349e25141a75546fcd5fe45f6abc6770
Jussi Toivola <jussi@jussi-laptop-vr.(none)>
2009-11-03 19:03:20 +0200

    Reimplemented binding using addToJavaScriptWindowObject

--- maemo/main.cpp
+++ maemo/main.cpp
@@ -11,100 +11,57 @@
 #include <QTextStream>
 #include <QWebFrame>
 
-class PGBaseCommand {
+#include "basecommand.h"
 
-    public:
-    PGBaseCommand(  ){
-        id = -1;
-    }
 
-    ~PGBaseCommand(){}
-    public:
-        void setUrl(QUrl aUrl )
-        {
-            iUrl = aUrl;
-        }
-        void execute()
-        {
-            std::cout << "Executing hello" << std::endl;
-
-            QMessageBox msgBox;
-            msgBox.setText("Hello world!");
-            msgBox.exec();
-        }
-
-        int id;
-    private:
-        QUrl iUrl;
-};
-
-class PGNetworkAccessManager : public QNetworkAccessManager
+PGBaseCommand::PGBaseCommand(QObject* parent) : QObject(parent)
 {
-    public:
-        PGNetworkAccessManager(QWebView *webview)
-        {
-            this->iWebView = webview;
-            iCommand.id = 0;
+}
 
-            initCommandMap();
-        }
-        ~PGNetworkAccessManager()
-        {
-            iCommandMap.clear();
-        }
+void PGBaseCommand::execute()
+{
+    std::cout << "Executing hello" << std::endl;
 
-        void initCommandMap()
-        {
-            iCommandMap.insert( QString().fromAscii("gaphello"), iCommand );
-        }
-    protected:
-        QNetworkReply *createRequest(Operation op, const QNetworkRequest &request,
-                                         QIODevice *outgoingData = 0)
-        {
-            std::cout << "createRequest:" << request.url().scheme().toAscii().data() << std::endl;
-            std::cout << "createRequest:" << request.url().toString().toAscii().data() << std::endl;
-            std::cout << "createRequest:" << request.url().path().remove(0,1).toAscii().data() << std::endl;
+    QMessageBox msgBox;
+    msgBox.setText("Hello world!");
+    msgBox.exec();
+}
 
-            // The scheme is 'gap<command>
-            PGBaseCommand command = iCommandMap.value( request.url().scheme() );
-            if( command.id != -1 )
-            {
-                command.execute();
-            }
-            else if ( request.url().scheme() == QString::fromAscii("file",4) )
-            {
-                qDebug("Running JS");
-                QFile file(request.url().path().remove(0,1) );
-                if (file.open(QIODevice::ReadOnly | QIODevice::Text))
-                {
-                    QTextStream in(&file);
-                    iWebView->page()->mainFrame()->evaluateJavaScript(in.readAll());
-                }
-            }
 
-            // TODO: How to make the response ourself
-            return QNetworkAccessManager::createRequest(op, request, outgoingData);
-        }
+#include "commandmanager.h"
 
-    private:
+void PGNetworkAccessManager::initJavascript( )
+{
+    std::cout << "initJavaScript" << std::endl;
+    this->iWebView->page()->mainFrame()->addToJavaScriptWindowObject(QString::fromAscii("Hello"), iCommand );
 
-        PGBaseCommand iCommand;
+}
 
-        // Scheme to command mapping
-        QHash<QString, PGBaseCommand> iCommandMap;
-        
-        // Reference to the web view
-        QWebView *iWebView;
 
-};
+void PGNetworkAccessManager::initCommandMap()
+{
+    // Create and bind the commands to javascript
+    iCommand = new PGBaseCommand(this->iWebView);
+    connect(this->iWebView->page()->mainFrame(), SIGNAL(javaScriptWindowObjectCleared()), this, SLOT(initJavascript() ));
+}
 
+PGNetworkAccessManager::PGNetworkAccessManager(QWebView *webview)
+{
+    this->iWebView = webview;
+    initCommandMap();
+}
+
+PGNetworkAccessManager::~PGNetworkAccessManager()
+{
+    delete iCommand;
+}
 
 /** Custom QWebPage which prints the javascript errors to console */
 class PGWebPage : public QWebPage
 {
     public:
 
-    virtual void javaScriptConsoleMessage(const QString& message, int lineNumber, const QString& sourceID)
+    void javaScriptConsoleMessage(const QString& message, int lineNumber, const QString& sourceID)
     {
         std::cout << lineNumber << ":" << sourceID.toAscii().data() << " " << message.toAscii().data() << std::endl;
     }
@@ -122,30 +79,25 @@ int main(int argc, char *argv[])
 
     // Construct the web view components
     QWebView *view = new QWebView(&window);
-    PGWebPage page;
-    PGNetworkAccessManager mymanager(view);
-
-    view->settings()->setAttribute(QWebSettings::JavascriptEnabled, TRUE);
 
     // Set our custom page with better error reporting
-    view->setPage(&page);
-    view->page()->setNetworkAccessManager(&mymanager);
+    // Has to be dynamically allocated because the QWebView deletes this.
+    PGWebPage* page = new PGWebPage();
+    view->setPage(page);
 
-    // To allow our 'gap' scheme
-    view->page()->setForwardUnsupportedContent(TRUE);
+    // Enable JS
+    view->settings()->setAttribute(QWebSettings::JavascriptEnabled, TRUE);
 
-    const QString path = QString::fromAscii("index.html");
+    PGNetworkAccessManager mymanager(view);
+    page->setNetworkAccessManager(&mymanager);
 
     QFile file("index.html");
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
          return 0;
 
     QTextStream in(&file);
-    
-    //const char* html = "<html><body><a href='gap://hello?world'>HELLO!</a></body></html>";
 
     view->setHtml( in.readAll(), QUrl("file:///index.html") );
-
     view->show();
 
     return app.exec();
