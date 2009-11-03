553752d39ab8116eeec22d7ad3f4760c2f1f2a71
Fil Maj <filip.maj@nitobi.com>
2009-11-03 13:08:39 -0800

    Fixed bad semi-colon / line breaks in javascripts which caused build to fail.

--- javascripts/blackberry/media.js
+++ javascripts/blackberry/media.js
@@ -1,15 +1,15 @@
 Media.prototype.record = function() {
 	alert('Media recording not implemented - yet.');
-}
+};
 
 Media.prototype.play = function() {
 	PhoneGap.exec("media",[this.src]);
-}
+};
 
 Media.prototype.pause = function() {
 	alert('Media pausing not implemented - yet.');
-}
+};
 
 Media.prototype.stop = function() {
 	alert('Media stopping not implemented - yet.');
-}
+};
--- javascripts/blackberry/sms.js
+++ javascripts/blackberry/sms.js
@@ -1,1 +1,2 @@
+
 // TODO: Have to implement SMS!
