f469faefb9ae1d187f0d793950606591fd39ffc2
Michael Brooks <michael@michaelbrooks.ca>
2010-03-24 13:32:30 -0700

    Fixed issue #51: UIWebView Viewport Larger than Application Viewport.
    Use the applicationFrame's size and the screenBounds' origin to construct a frame for the window.
    This has been tested with the tab bar and it works correctly.

--- PhoneGapLib/Classes/PhoneGapDelegate.m
+++ PhoneGapLib/Classes/PhoneGapDelegate.m
@@ -92,8 +92,15 @@ static NSString *gapVersion;
  */
 - (void)applicationDidFinishLaunching:(UIApplication *)application
 {	
-	
-	CGRect screenBounds = [ [ UIScreen mainScreen ] bounds ];
+	/* 
+	 * The applicationFrame size (viewport) is correct   because it accounts for the status bar.
+	 * The applicationFrame origin          is incorrect because it is offset by the toolbar (non-existant).
+	 * The mainScreen       origin          is correct   because it is not offset and is directly under the status bar.
+	 */
+	CGRect screenBounds      = [ [ UIScreen mainScreen ] bounds ];
+	CGRect applicationBounds = [ [ UIScreen mainScreen ] applicationFrame ];
+	screenBounds.size = applicationBounds.size;
+
 	self.window = [ [ [ UIWindow alloc ] initWithFrame:screenBounds ] autorelease ];
 	viewController = [ [ PhoneGapViewController alloc ] init ];
 	
