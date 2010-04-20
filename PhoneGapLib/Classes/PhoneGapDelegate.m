d5ddfd8cbc0ea90bbd9d4bca69a796336ee2665f
Jesse MacFadyen <jesse.macfadyen@nitobi.com>
2010-04-20 15:27:19 -0700

    Removed ref to Device::PhoneGapCommand as it was not a 'true' command and had a reverse dependency on PhoneGapDelegate.

--- PhoneGapLib/Classes/PhoneGapDelegate.m
+++ PhoneGapLib/Classes/PhoneGapDelegate.m
@@ -243,6 +243,20 @@ static NSString *gapVersion;
     }
 }
 
+- (NSDictionary*) deviceProperties
+{
+	UIDevice *device = [UIDevice currentDevice];
+    NSMutableDictionary *devProps = [NSMutableDictionary dictionaryWithCapacity:4];
+    [devProps setObject:[device model] forKey:@"platform"];
+    [devProps setObject:[device systemVersion] forKey:@"version"];
+    [devProps setObject:[device uniqueIdentifier] forKey:@"uuid"];
+    [devProps setObject:[device name] forKey:@"name"];
+    [devProps setObject:[PhoneGapDelegate phoneGapVersion ] forKey:@"gap"];
+	
+    NSDictionary *devReturn = [NSDictionary dictionaryWithDictionary:devProps];
+    return devReturn;
+}
+
 - (NSString*) appURLScheme
 {
 	// The info.plist contains this structure:
@@ -308,7 +322,7 @@ static NSString *gapVersion;
 	 * Hide the Top Activity THROBER in the Battery Bar
 	 */
 	
-	NSDictionary *deviceProperties = [[self getCommandInstance:@"Device"] deviceProperties];
+    NSDictionary *deviceProperties = [ self deviceProperties];
     NSMutableString *result = [[NSMutableString alloc] initWithFormat:@"DeviceInfo = %@;", [deviceProperties JSONFragment]];
     
     /* Settings.plist
