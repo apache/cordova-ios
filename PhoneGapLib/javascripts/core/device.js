c1857debefd9198fdf4e9a03ec5ac02e3a5aa98a
macdonst <simon.macdonald@gmail.com>
2010-09-23 04:48:13 +0800

    Changing device.gap to device.phonegap

--- PhoneGapLib/javascripts/core/device.js
+++ PhoneGapLib/javascripts/core/device.js
@@ -8,14 +8,14 @@ function Device()
     this.platform = null;
     this.version  = null;
     this.name     = null;
-    this.gap      = null;
+    this.phonegap      = null;
     this.uuid     = null;
     try 
 	{      
 		this.platform = DeviceInfo.platform;
 		this.version  = DeviceInfo.version;
 		this.name     = DeviceInfo.name;
-		this.gap      = DeviceInfo.gap;
+		this.phonegap = DeviceInfo.gap;
 		this.uuid     = DeviceInfo.uuid;
 
     } 
