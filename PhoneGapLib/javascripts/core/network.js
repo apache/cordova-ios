4875c25635a52d07b0377d36d4081ad640462128
shazron <shazron@gmail.com>
2011-07-07 14:23:28 -0700

    Removed JavaScript interface for #157

--- PhoneGapLib/javascripts/core/network.js
+++ PhoneGapLib/javascripts/core/network.js
@@ -20,47 +20,13 @@ Connection = function() {
 Connection.UNKNOWN = "unknown"; // Unknown connection type
 Connection.ETHERNET = "ethernet";
 Connection.WIFI = "wifi";
-Connection.CELL_2G = "2g";
+Connection.CELL_2G = "2g"; // the default for iOS, for any cellular connection
 Connection.CELL_3G = "3g";
 Connection.CELL_4G = "4g";
 Connection.NONE = "none"; // NO connectivity
 
-// //////////////////////////////////////////////////////////////////
-	
-/**
- * This class contains information about any NetworkStatus.
- * @constructor
- */
-NetworkStatus = function() {
-	this.code = null;
-	this.message = "";
-}
-
-NetworkStatus.NOT_REACHABLE = 0;
-NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK = 1;
-NetworkStatus.REACHABLE_VIA_WIFI_NETWORK = 2;
-
-/**
- * This class provides access to device Network data (reachability).
- * @constructor
- */
-Network = function() {
-};
-
-/**
- * 
- * @param {Function} successCallback
- * @param {Function} errorCallback
- * @param {Object} options
- */
-Network.prototype.isReachable = function(hostName, successCallback, options) {
-	console.warn("Network.isReachable is deprecated and will be removed in 1.0. Please use the Network Information API instead.");
-	PhoneGap.exec("Network.isReachable", hostName, GetFunctionName(successCallback), options);
-};
-
 
 PhoneGap.addConstructor(function() {
-    if (typeof navigator.network == "undefined") navigator.network = new Network();
     if (typeof navigator.network.connection == "undefined") navigator.network.connection = new Connection();
 });
 
