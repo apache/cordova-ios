7f22a6fd23050527cd86af69d878380f0dd98e30
shazron <shazron@gmail.com>
2011-07-09 01:03:20 -0700

    Fixed PGDebugConsole log method was printing the callbackId, instead of the message

--- PhoneGapLib/Classes/DebugConsole.m
+++ PhoneGapLib/Classes/DebugConsole.m
@@ -12,7 +12,9 @@
 
 - (void)log:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
 {
-    NSString* message = [arguments objectAtIndex:0];
+	NSString* callbackId = [arguments objectAtIndex:0];
+#pragma unused(callbackId)
+    NSString* message = [arguments objectAtIndex:1];
     NSString* log_level = @"INFO";
     if ([options objectForKey:@"logLevel"])
         log_level = [options objectForKey:@"logLevel"];
