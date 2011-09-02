8e13f31969a4a2fbfc10f6770bbb152c2a08ce7f
brianleroux <brian@westcoastlogic.com>
2011-09-02 14:10:29 -0700

    begin restructuring for cli scripts

---
+++ bin/templates/project/__TESTING__/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  __TESTING__
+//
+//  Created by Brian LeRoux on 11-08-24.
+//  Copyright Nitobi Software 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit __TESTING__.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
