489e8d315c72e85df2b233aa57066c3d6e0429b6
Becky Gibson <gibson.becky@gmail.com>
2011-09-29 05:06:47 +0800

    fix #289 update contact to deal with an address with no type specified

--- PhoneGapLib/Classes/Contact.m
+++ PhoneGapLib/Classes/Contact.m
@@ -554,7 +554,11 @@ static NSDictionary*	com_phonegap_contacts_defaultFields = nil;
 		if (newDict){  // create a new dictionary with a Label and Value, value is the dictionary previously created
             // June, 2011 W3C Contact spec adds type into ContactAddress book
             // get the type out of the original dictionary for address
-			NSObject* typeValue = ((prop == kABPersonInstantMessageProperty) ? (NSObject*)kABOtherLabel : (NSString*)[dict valueForKey: kW3ContactFieldType]);
+            NSString* addrType = (NSString*)[dict valueForKey: kW3ContactFieldType];
+            if (!addrType) {
+                addrType =  (NSString*) kABOtherLabel;
+            }
+			NSObject* typeValue = ((prop == kABPersonInstantMessageProperty) ? (NSObject*)kABOtherLabel : addrType );
             //NSLog(@"typeValue: %@", typeValue);
 			[addDict setObject: typeValue forKey: kW3ContactFieldType];  //  im labels will be set as Other and address labels as type from dictionary
 			[addDict setObject: newDict forKey:kW3ContactFieldValue];
