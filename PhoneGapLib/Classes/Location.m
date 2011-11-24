a17eee6a80208550aa7f3578b07e061b89cc8169
shazron <shazron@gmail.com>
2011-11-24 11:31:49 -0800

    Fixed broken build.
    This is due to errant closing curly brace (from pull-request callback/callback-ios#14)

--- PhoneGapLib/Classes/Location.m
+++ PhoneGapLib/Classes/Location.m
@@ -176,9 +176,6 @@
         return;
     }
 	
-    // Tell the location manager to start notifying us of location updates
-	}
-    
     // Tell the location manager to start notifying us of location updates. We
     // first stop, and then start the updating to ensure we get at least one
     // update, even if our location did not change.
