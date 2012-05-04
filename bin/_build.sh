4c7a02e092023890f4a56bc67190c19652f6d1c9
Shazron Abdullah <shazron@apache.org>
2012-05-03 17:33:15 -0700

    Added Apache License to all the things

--- bin/_build.sh
+++ bin/_build.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+
 # iPhone OS Device SDKs:
 # 	Device - iPhone OS 2.2.1      	-sdk iphoneos2.2.1
 # 	Device - iPhone OS 3.0        	-sdk iphoneos3.0
@@ -58,15 +78,15 @@ configuration=$first_letter$non_first_letter_substring
 
 # Check whether the xcode project path exists
 if [ ! -d $xcodeproj_folder ]; then
-	echo "Path to xcode folder '$xcodeproj_folder' not found."
+	echo "Path to Xcode folder '$xcodeproj_folder' not found."
 	exit
 fi
 
-echo 'PhoneGap: building...'
+echo 'Cordova: building...'
 
 # change to the project directory, and run the build
 cd $xcodeproj_folder
 echo $xcodebuild -alltargets -configuration $configuration $sdk
 $xcodebuild -alltargets -configuration $configuration $sdk VALID_ARCHS="$archs"
 
-echo 'PhoneGap: build done.'
+echo 'Cordova: build done.'
