681016b3e0e7e006657406fc0158aaa8515c3369
Shazron Abdullah <shazron@apache.org>
2012-09-17 19:28:28 -0700

    [CB-1465] WebView too small after closing of a ChildBrowser in landscape orientation

--- bin/templates/project/__TESTING__/Classes/MainViewController.m
+++ bin/templates/project/__TESTING__/Classes/MainViewController.m
@@ -52,8 +52,13 @@
 {
     // Set the main view to utilize the entire application frame space of the device.
     // Change this to suit your view's UI footprint needs in your application.
-    self.view.frame = [[UIScreen mainScreen] applicationFrame];
-    
+
+    UIView* rootView =[[[[UIApplication sharedApplication] keyWindow] rootViewController] view];
+    CGRect webViewFrame = [[[rootView subviews] objectAtIndex:0] frame];  // first subview is the UIWebView
+    if (CGRectEqualToRect(webViewFrame, CGRectZero)) { // UIWebView is sized according to its parent, here it hasn't been sized yet
+        self.view.frame = [[UIScreen mainScreen] applicationFrame]; // size UIWebView's parent according to application frame, which will in turn resize the UIWebView
+    }
+
     [super viewWillAppear:animated];
 }
 
