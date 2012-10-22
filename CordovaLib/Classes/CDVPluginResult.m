643e5cf8ef84d10fee3acfcba376bd3f4961772e
Kevin Hawkins <khawkins@salesforce.com>
2012-10-21 20:49:01 -0700

    Adding bool plugin result message, tests

--- CordovaLib/Classes/CDVPluginResult.m
+++ CordovaLib/Classes/CDVPluginResult.m
@@ -88,6 +88,11 @@ static NSArray* org_apache_cordova_CommandStatusMsgs;
     return [[self alloc] initWithStatus:statusOrdinal message:[NSNumber numberWithDouble:theMessage]];
 }
 
++ (CDVPluginResult*)resultWithStatus:(CDVCommandStatus)statusOrdinal messageAsBool:(BOOL)theMessage
+{
+    return [[self alloc] initWithStatus:statusOrdinal message:[NSNumber numberWithBool:theMessage]];
+}
+
 + (CDVPluginResult*)resultWithStatus:(CDVCommandStatus)statusOrdinal messageAsDictionary:(NSDictionary*)theMessage
 {
     return [[self alloc] initWithStatus:statusOrdinal message:theMessage];
