46577a9e440d6a3052c83f98b9d796c6b98278c8
Shazron Abdullah <shazron@apache.org>
2012-10-24 13:42:50 -0700

    Updated js to 2.2.0rc2

--- bin/templates/project/www/cordova-2.2.0rc2.js
+++ bin/templates/project/www/cordova-2.2.0rc2.js
@@ -1,6 +1,6 @@
-// commit 98d190ad98ec48f4f06c61286faee99c0eb000ed
+// commit 6881d72b96f476a1e8cd6e03fe1465a0fb88b000
 
-// File generated at :: Mon Oct 15 2012 12:53:57 GMT-0700 (PDT)
+// File generated at :: Wed Oct 24 2012 13:39:22 GMT-0700 (PDT)
 
 /*
  Licensed to the Apache Software Foundation (ASF) under one
@@ -695,7 +695,6 @@ module.exports = {
         return ( CommandProxyMap[service] ? CommandProxyMap[service][action] : null );
     }
 };
-
 });
 
 // file: lib/common/common.js
@@ -5921,8 +5920,8 @@ utils.extend = (function() {
  * Alerts a message in any available way: alert or console.log.
  */
 utils.alert = function(msg) {
-    if (alert) {
-        alert(msg);
+    if (window.alert) {
+        window.alert(msg);
     } else if (console && console.log) {
         console.log(msg);
     }
