d0837cc8b7fbf0cef2adee159f15fea697d0249a
Andrew Grieve <agrieve@chromium.org>
2012-10-30 22:36:59 -0400

    Add more comments to CDVCommandDelegate.h.

--- CordovaLib/Classes/CDVCommandDelegate.h
+++ CordovaLib/Classes/CDVCommandDelegate.h
@@ -33,12 +33,16 @@
 // will result in bogus callbacks being made.
 - (BOOL)execute:(CDVInvokedUrlCommand*)command CDV_DEPRECATED(2.2, "Use direct method calls instead.");
 
-// Sends a plugin result to the JS.
+// Sends a plugin result to the JS. This is thread-safe.
 - (void)sendPluginResult:(CDVPluginResult*)result callbackId:(NSString*)callbackId;
-// Evaluates the given JS.
+// Evaluates the given JS. This is thread-safe.
 - (void)evalJs:(NSString*)js;
+// Can be used to evaluate JS right away instead of scheduling it on the run-loop.
+// This is required for dispatch resign and pause events, but should not be used
+// without reason. Without the run-loop delay, alerts used in JS callbacks may result
+// in dead-lock. This method must be called from the UI thread.
 - (void)evalJs:(NSString*)js scheduledOnRunLoop:(BOOL)scheduledOnRunLoop;
-// Runs the given block on a background thread.
+// Runs the given block on a background thread using a shared thread-pool.
 - (void)runInBackground:(void (^)())block;
 
 @end
