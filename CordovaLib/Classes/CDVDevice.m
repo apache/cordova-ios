093c98263b59bff560bb4e3e6ebd1d0bbac4309e
Shazron Abdullah <shazron@apache.org>
2012-11-27 14:01:38 -0800

    [CB-1850] Add device.model to the Device API

--- CordovaLib/Classes/CDVDevice.m
+++ CordovaLib/Classes/CDVDevice.m
@@ -17,7 +17,27 @@
  under the License.
  */
 
+#include <sys/types.h>
+#include <sys/sysctl.h>
+
 #import "CDV.h"
+
+@implementation UIDevice (ModelVersion)
+
+- (NSString*)modelVersion
+{
+    size_t size;
+
+    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+    char* machine = malloc(size);
+    sysctlbyname("hw.machine", machine, &size, NULL, 0);
+    NSString* platform = [NSString stringWithUTF8String:machine];
+    free(machine);
+
+    return platform;
+}
+
+@end
 
 @interface CDVDevice () {}
 @end
@@ -50,6 +70,7 @@
     UIDevice* device = [UIDevice currentDevice];
     NSMutableDictionary* devProps = [NSMutableDictionary dictionaryWithCapacity:4];
 
+    [devProps setObject:[device modelVersion] forKey:@"model"];
     [devProps setObject:@"iOS" forKey:@"platform"];
     [devProps setObject:[device systemVersion] forKey:@"version"];
     [devProps setObject:[device uniqueAppInstanceIdentifier] forKey:@"uuid"];
