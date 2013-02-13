81e3332adaa6524da20099a68ed7625c5884cf89
Andrew Grieve <agrieve@chromium.org>
2013-02-13 16:11:46 -0500

    [CB-2433] Deprecation notice for window.Settings

--- CordovaLib/Classes/CDVDevice.m
+++ CordovaLib/Classes/CDVDevice.m
@@ -58,6 +58,7 @@
     NSDictionary* temp = [CDVViewController getBundlePlist:@"Settings"];
 
     if ([temp respondsToSelector:@selector(JSONString)]) {
+        NSLog(@"Deprecation warning: window.Setting will be removed Aug 2013. Refer to https://issues.apache.org/jira/browse/CB-2433");
         NSString* js = [NSString stringWithFormat:@"window.Settings = %@;", [temp JSONString]];
         [self.commandDelegate evalJs:js];
     }
