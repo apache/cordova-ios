dfc6a993a99a701cc153178c6826a77727aca4df
Max Woghiren <max.woghiren@gmail.com>
2013-02-15 12:53:37 -0500

    [CB-2411] Added camera popover repositioning.
    Also prevented the possibility of having multiple popovers from multiple
    calls to Camera.getPicture.

--- CordovaLib/Classes/CDVCamera.h
+++ CordovaLib/Classes/CDVCamera.h
@@ -80,6 +80,7 @@ typedef NSUInteger CDVMediaType;
 - (void)takePicture:(CDVInvokedUrlCommand*)command;
 - (void)postImage:(UIImage*)anImage withFilename:(NSString*)filename toUrl:(NSURL*)url;
 - (void)cleanup:(CDVInvokedUrlCommand*)command;
+- (void)repositionPopover:(CDVInvokedUrlCommand*)command;
 
 - (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingMediaWithInfo:(NSDictionary*)info;
 - (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingImage:(UIImage*)image editingInfo:(NSDictionary*)editingInfo;
