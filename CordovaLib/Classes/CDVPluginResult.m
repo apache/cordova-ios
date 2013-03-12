b6e3bb46f0c342304adeb6702c2903e2c27afe7f
Andrew Grieve <agrieve@chromium.org>
2013-03-11 22:04:29 -0400

    Add an associatedObject field to CDVPluginResult.
    Could have used objc_setAssociatedObject, but this seems simpler since
    we own the class.

--- CordovaLib/Classes/CDVPluginResult.m
+++ CordovaLib/Classes/CDVPluginResult.m
@@ -29,16 +29,16 @@
 @end
 
 @implementation CDVPluginResult
-@synthesize status, message, keepCallback;
+@synthesize status, message, keepCallback, associatedObject;
 
 static NSArray* org_apache_cordova_CommandStatusMsgs;
 
 id messageFromArrayBuffer(NSData* data)
 {
-    return @{
-        @"CDVType" : @"ArrayBuffer",
-        @"data" :[data base64EncodedString]
-    };
+    return @ {
+               @"CDVType" : @"ArrayBuffer",
+               @"data" :[data base64EncodedString]
+    }
 }
 
 id massageMessage(id message)
@@ -57,10 +57,10 @@ id messageFromMultipart(NSArray* theMessages)
         [messages replaceObjectAtIndex:i withObject:massageMessage([messages objectAtIndex:i])];
     }
 
-    return @{
-        @"CDVType" : @"MultiPart",
-        @"messages" : messages
-    };
+    return @ {
+               @"CDVType" : @"MultiPart",
+               @"messages" : messages
+    }
 }
 
 + (void)initialize
@@ -141,7 +141,7 @@ id messageFromMultipart(NSArray* theMessages)
 
 + (CDVPluginResult*)resultWithStatus:(CDVCommandStatus)statusOrdinal messageToErrorObject:(int)errorCode
 {
-    NSDictionary* errDict = @{@"code": [NSNumber numberWithInt:errorCode]};
+    NSDictionary* errDict = @ {@"code" :[NSNumber numberWithInt:errorCode]}
 
     return [[self alloc] initWithStatus:statusOrdinal message:errDict];
 }
