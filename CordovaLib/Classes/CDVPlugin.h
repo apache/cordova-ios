9facafb4f96c7f81097a9424505398821589ba54
Shazron Abdullah <shazron@apache.org>
2013-04-11 13:06:23 -0700

    [CB-3021] Can no longer import CDVPlugin.h from plugin Objective-C++ code

--- CordovaLib/Classes/CDVPlugin.h
+++ CordovaLib/Classes/CDVPlugin.h
@@ -23,10 +23,10 @@
 #import "NSMutableArray+QueueAdditions.h"
 #import "CDVCommandDelegate.h"
 
-NSString* const CDVPageDidLoadNotification;
-NSString* const CDVPluginHandleOpenURLNotification;
-NSString* const CDVPluginResetNotification;
-NSString* const CDVLocalNotification;
+extern NSString* const CDVPageDidLoadNotification;
+extern NSString* const CDVPluginHandleOpenURLNotification;
+extern NSString* const CDVPluginResetNotification;
+extern NSString* const CDVLocalNotification;
 
 @interface CDVPlugin : NSObject {}
 
