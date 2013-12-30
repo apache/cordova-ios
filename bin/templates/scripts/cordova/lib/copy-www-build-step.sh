806651d674a970a7d22ed9c3132f206d88594f0c
Andrew Grieve <agrieve@chromium.org>
2013-12-30 15:21:10 -0500

    Copy config.xml within copy-www-build-step.sh instead of in Copy Resoruces step
    Related to CB-5397.
    This will allow users to delete the "Staging" group within a CLI project
    without breaking their builds. They may want to do this so that there
    isn't any confusion when between staging vs. non-staging when doing a
    Quick-Open (Command-Shift-O).

--- bin/templates/scripts/cordova/lib/copy-www-build-step.sh
+++ bin/templates/scripts/cordova/lib/copy-www-build-step.sh
@@ -29,6 +29,10 @@ COPY_HIDDEN=
 ORIG_IFS=$IFS
 IFS=$(echo -en "\n\b")
 
+if [[ -z "$BUILT_PRODUCTS_DIR" ]]; then
+  echo "The script is meant to be run as an Xcode build step and relies on env variables set by Xcode."
+  exit 1
+fi
 if [[ ! -e "$SRC_DIR" ]]; then
   echo "Path does not exist: $SRC_DIR"
   exit 1
@@ -73,6 +77,9 @@ for p in $(do_find -type f -print); do
     rsync -a "$SRC_DIR$subpath" "$DST_DIR$subpath" || exit 4
   fi
 done
+
+# Copy the config.xml file.
+cp -f "${PROJECT_FILE_PATH%.xcodeproj}/config.xml" "$BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME"
 
 )
 IFS=$ORIG_IFS
