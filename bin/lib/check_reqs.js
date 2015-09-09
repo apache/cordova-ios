7e68636c93b32d3079fe22c90bf05deb1ab85d56
Simon MacDonald <simon.macdonald@gmail.com>
2015-09-09 00:00:09 -0400

    CB-9328 Use ios-sim as a node module, not a CLI utility

--- bin/lib/check_reqs.js
+++ bin/lib/check_reqs.js
@@ -25,11 +25,6 @@ var XCODEBUILD_MIN_VERSION = '6.0.0';
 var XCODEBUILD_NOT_FOUND_MESSAGE =
     'Please install version ' + XCODEBUILD_MIN_VERSION + ' or greater from App Store';
 
-var IOS_SIM_MIN_VERSION = '4.0.0';
-var IOS_SIM_NOT_FOUND_MESSAGE =
-    'Please download, build and install version ' + IOS_SIM_MIN_VERSION + ' or greater' +
-    ' from https://github.com/phonegap/ios-sim into your path, or do \'npm install -g ios-sim\'';
-
 var IOS_DEPLOY_MIN_VERSION = '1.7.0';
 var IOS_DEPLOY_NOT_FOUND_MESSAGE =
     'Please download, build and install version ' + IOS_DEPLOY_MIN_VERSION + ' or greater' +
@@ -51,14 +46,6 @@ module.exports.check_ios_deploy = function () {
     return checkTool('ios-deploy', IOS_DEPLOY_MIN_VERSION, IOS_DEPLOY_NOT_FOUND_MESSAGE);
 };
 
-/**
- * Checks if ios-sim util is available
- * @return {Promise} Returns a promise either resolved with ios-sim version or rejected
- */
-module.exports.check_ios_sim = function () {
-    return checkTool('ios-sim', IOS_SIM_MIN_VERSION, IOS_SIM_NOT_FOUND_MESSAGE);
-};
-
 module.exports.check_os = function () {
     // Build iOS apps available for OSX platform only, so we reject on others platforms
     return process.platform === 'darwin' ?
@@ -72,7 +59,7 @@ module.exports.help = function () {
 
 /**
  * Checks if specific tool is available.
- * @param  {String} tool       Tool name to check. Known tools are 'xcodebuild', 'ios-sim' and 'ios-deploy'
+ * @param  {String} tool       Tool name to check. Known tools are 'xcodebuild' and 'ios-deploy'
  * @param  {Number} minVersion Min allowed tool version.
  * @param  {String} message    Message that will be used to reject promise.
  * @return {Promise}           Returns a promise either resolved with tool version or rejected
@@ -119,8 +106,7 @@ module.exports.check_all = function() {
     var requirements = [
         new Requirement('os', 'Apple OS X', true),
         new Requirement('xcode', 'Xcode'),
-        new Requirement('ios-deploy', 'ios-deploy'),
-        new Requirement('ios-sim', 'ios-sim')
+        new Requirement('ios-deploy', 'ios-deploy')
     ];
 
     var result = [];
@@ -129,8 +115,7 @@ module.exports.check_all = function() {
     var checkFns = [
         module.exports.check_os,
         module.exports.check_xcodebuild,
-        module.exports.check_ios_deploy,
-        module.exports.check_ios_sim
+        module.exports.check_ios_deploy
     ];
 
     // Then execute requirement checks one-by-one
