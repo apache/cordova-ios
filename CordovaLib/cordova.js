8af2961a28c74d5f455fff14c82020d6a76904f5
Shazron Abdullah <shazron@apache.org>
2015-12-04 17:05:25 -0800

    Update JS snapshot to version 4.0.0 (via coho)

--- CordovaLib/cordova.js
+++ CordovaLib/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '4.0.0-dev';
+var PLATFORM_VERSION_BUILD_LABEL = '4.0.0';
 // file: src/scripts/require.js
 
 /*jshint -W079 */
