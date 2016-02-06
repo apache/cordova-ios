d0521fe28d3c20054908d229298ba9ced1033c92
Ben Connito <ben.connito@exponentialent.com>
2016-02-06 10:58:17 -0500

    CB-10184: Images.xcassets: A 83.5x83.5@2x app icon is required for iPad apps targeting iOS 9.0 and later
    added image to project template
    added destination to prepare.js
    
    added to ipad pro image definition to contents.json

--- bin/templates/scripts/cordova/lib/prepare.js
+++ bin/templates/scripts/cordova/lib/prepare.js
@@ -280,7 +280,8 @@ function handleIcons(projectConfig, platformRoot) {
         {dest: 'icon-72.png', width: 72, height: 72},
         {dest: 'icon-72@2x.png', width: 144, height: 144},
         {dest: 'icon-50.png', width: 50, height: 50},
-        {dest: 'icon-50@2x.png', width: 100, height: 100}
+        {dest: 'icon-50@2x.png', width: 100, height: 100},
+				{dest: 'icon-83.5@2x.png', width: 167, height: 167}
     ];
 
     var destIconsFolder;
