820c29417149620b2c83dbe6f76162e02ed352f0
Shazron Abdullah <shazron@apache.org>
2016-02-25 17:58:40 -0800

    CB-10563 - Remove outdated text in CDVPlugin header

--- CordovaLib/Classes/Public/CDVPlugin.h
+++ CordovaLib/Classes/Public/CDVPlugin.h
@@ -64,26 +64,6 @@ extern NSString* const CDVRemoteNotificationError;
  - (void)didReceiveLocalNotification:(NSNotification *)notification;
  */
 
- /*
- // These methods may optionally be implemented. If present, they will be called by CVDViewController when network-policy (whitelist) decisions need to be made.
-
-    This method, if present, will be called by CDVURLProtocol to determine whether to block a request for a web resource. If all plugins which implement this method return true, then the request will be allowed. If any plugin returns false, the request will be blocked. If no plugins implement this method, then the default policy will be followed. (See [CDVViewController defaultResourcePolicyForURL:])
-
-    Note that this method will *not* be called for some resources, such as WebSocket connections, and will not be called by WKWebView at all. Please use CSP headers to handle those situations if needed.
-
- - (BOOL)shouldAllowRequestForURL:(NSURL *)url
-
-
-    This method, if present, will be called by CDVViewController to determine whether webview navigation to a web page should be allowed. If all plugins which implement this method return true, then the navigation will be allowed. If any plugin returns false, the navigation will be blocked. If no plugins implement this method, then the default policy will be followed. (See [CDVViewController defaultResourcePolicyForURL:])
-
- - (BOOL)shouldAllowNavigationToURL:(NSURL *)url
-
-
-    This method, if present, will be called by CDVViewController to determine whether a web page which is otherwise blocked should be opened in the system browser. If all plugins which implement this method return true, then the page will be opened. If any plugin returns false, the page will not be opened. If no plugins implement this method, then the default policy will be followed. (See [CDVViewController shouldOpenExternalURL:] and [CDVViewController webView:shouldStartLoadWithRequest:navigationType:])
-
- - (BOOL)shouldOpenExternalURL:(NSURL *)url
- */
-
 - (id)appDelegate;
 
 @end
