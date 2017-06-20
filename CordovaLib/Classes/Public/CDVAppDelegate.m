f49771bfa084b1b64965ee97a3b4afbf407693c3
Georgios Galatoulas <georgiosgalatoulas@gmail.com>
2017-06-20 13:35:40 +0100

    CB-12937 - added new method handleOpenURLWithApplicationSourceAndAnnotation in CDVPlugin and new notification id CDVPluginHandleOpenURLWithAppSourceAndAnnotationNotification which is broadcasted from CDVAppDelegate openURL method.

--- CordovaLib/Classes/Public/CDVAppDelegate.m
+++ CordovaLib/Classes/Public/CDVAppDelegate.m
@@ -78,9 +78,23 @@
     if (!url) {
         return NO;
     }
+    
+    NSMutableDictionary * openURLData = [[NSMutableDictionary alloc] init];
+    
+    [openURLData setValue:url forKey:@"url"];
+    
+    if (sourceApplication) {
+        [openURLData setValue:sourceApplication forKey:@"sourceApplication"];
+    }
+    
+    if (annotation) {
+        [openURLData setValue:annotation forKey:@"annotation"];
+    }
 
     // all plugins will get the notification, and their handlers will be called
     [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLNotification object:url]];
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLWithAppSourceAndAnnotationNotification object:openURLData]];
+
 
     return YES;
 }
