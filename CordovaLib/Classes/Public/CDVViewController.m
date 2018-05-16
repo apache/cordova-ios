00a4b27466bb8d1abf11e9e9ad6f2c6c1510f652
Shazron Abdullah <shaz@adobe.com>
2018-05-16 13:46:14 +0800

    Revert "CB-14045 - Reinit url after app freezes (#363)"
    This reverts commit 6b3f10f70ed0fe10ad792b49228a8fb6686082f1.

--- CordovaLib/Classes/Public/CDVViewController.m
+++ CordovaLib/Classes/Public/CDVViewController.m
@@ -720,33 +720,12 @@
     }
 }
 
-- (bool)isUrlEmpty:(NSURL *)url
-{
-    if (!url || (url == (id) [NSNull null])) {
-        return true;
-    }
-    NSString *urlAsString = [url absoluteString];
-    return (urlAsString == (id) [NSNull null] || [urlAsString length]==0 || [urlAsString isEqualToString:@"about:blank"]);
-}
-
-- (bool)checkAndReinitViewUrl
-{
-    NSURL* appURL = [self appUrl];
-    if ([self isUrlEmpty: [self.webViewEngine URL]] && ![self isUrlEmpty: appURL]) {
-        NSURLRequest* appReq = [NSURLRequest requestWithURL:appURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:20.0];
-        [self.webViewEngine loadRequest:appReq];
-        return true;
-    }
-    return false;
-}
-
 /*
  This method is called to let your application know that it is about to move from the active to inactive state.
  You should use this method to pause ongoing tasks, disable timer, ...
  */
 - (void)onAppWillResignActive:(NSNotification*)notification
 {
-    [self checkAndReinitViewUrl];
     // NSLog(@"%@",@"applicationWillResignActive");
     [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('resign');" scheduledOnRunLoop:NO];
 }
@@ -758,7 +737,6 @@
  */
 - (void)onAppWillEnterForeground:(NSNotification*)notification
 {
-    [self checkAndReinitViewUrl];
     // NSLog(@"%@",@"applicationWillEnterForeground");
     [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('resume');"];
 
@@ -773,7 +751,6 @@
 // This method is called to let your application know that it moved from the inactive to active state.
 - (void)onAppDidBecomeActive:(NSNotification*)notification
 {
-    [self checkAndReinitViewUrl];
     // NSLog(@"%@",@"applicationDidBecomeActive");
     [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('active');"];
 }
@@ -784,7 +761,6 @@
  */
 - (void)onAppDidEnterBackground:(NSNotification*)notification
 {
-    [self checkAndReinitViewUrl];
     // NSLog(@"%@",@"applicationDidEnterBackground");
     [self.commandDelegate evalJs:@"cordova.fireDocumentEvent('pause', null, true);" scheduledOnRunLoop:NO];
 }
