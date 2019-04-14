ce4fbc7ebe3f24d7d6af2696bf692bf0c7010b69
Jan Piotrowski <piotrowski+git@gmail.com>
2019-04-14 17:39:22 +0200

    fix eslint stuff

--- bin/templates/scripts/cordova/lib/run.js
+++ bin/templates/scripts/cordova/lib/run.js
@@ -204,13 +204,13 @@ function startSim (appPath, target) {
 
 function iossimLaunch (appPath, devicetypeid, log, exit) {
     var f = path.resolve(path.dirname(require.resolve('ios-sim')), 'bin', 'ios-sim');
-    
+
     var params = ['launch', appPath, '--devicetypeid', devicetypeid, '--log', log, exit];
-    console.log("$ " + f + ' ' + params.join(' '));
+    console.log('$ ' + f + ' ' + params.join(' '));
     var proc = cp.spawn(f, params);
-    
+
     proc.stdout.on('data', (data) => {
-        console.log("ios-sim log: " + data.toString());
+        console.log('ios-sim log: ' + data.toString());
     });
 }
 
