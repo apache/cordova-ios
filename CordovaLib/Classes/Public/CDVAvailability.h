45ae4d47221b2d077bc9a8b289358cce5e46764f
Darryl Pogue <darryl@ayogo.com>
2019-04-18 13:25:36 -0700

    Added 5.0.1 to CDVAvailability.h (via coho).

--- CordovaLib/Classes/Public/CDVAvailability.h
+++ CordovaLib/Classes/Public/CDVAvailability.h
@@ -72,6 +72,7 @@
 #define __CORDOVA_4_5_2 40502
 #define __CORDOVA_4_5_4 40504
 #define __CORDOVA_5_0_0 50000
+#define __CORDOVA_5_0_1 50001
 /* coho:next-version,insert-before */
 #define __CORDOVA_NA 99999      /* not available */
 
@@ -84,7 +85,7 @@
  */
 #ifndef CORDOVA_VERSION_MIN_REQUIRED
     /* coho:next-version-min-required,replace-after */
-    #define CORDOVA_VERSION_MIN_REQUIRED __CORDOVA_5_0_0
+    #define CORDOVA_VERSION_MIN_REQUIRED __CORDOVA_5_0_1
 #endif
 
 /*
