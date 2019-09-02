35384b53fea206249363e33736f9bf83222ef860
Bertrand Presles <bpresles@gmail.com>
2019-09-03 01:50:17 +0200

    Finished integration of WKWebview JS code

--- cordova-js-src/exec.js
+++ cordova-js-src/exec.js
@@ -165,12 +165,3 @@ execProxy.nativeCallback = function () {
 };
 
 module.exports = execProxy;
-
-/*var cexec = require('cordova/exec');
-var WkWebKit = {
-    allowsBackForwardNavigationGestures: function (allow) {
-        cexec(null, null, 'CDVWKWebViewEngine', 'allowsBackForwardNavigationGestures', [allow]);
-    }
-};
-
-module.exports = WkWebKit; */
