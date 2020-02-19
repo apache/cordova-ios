fc1f915169c599f5bbbffd07456e1df0a61037d7
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2020-02-19 02:47:30 -0500

    fix & improve replacement of ternary operator

--- bin/templates/scripts/cordova/lib/projectFile.js
+++ bin/templates/scripts/cordova/lib/projectFile.js
@@ -50,7 +50,8 @@ function parseProjectFile (locations) {
         return (
             entry.buildSettings &&
             entry.buildSettings.INFOPLIST_FILE &&
-            !(projectName && entry.buildSettings.INFOPLIST_FILE.includes(`${projectName}/${projectName}-Info.plist`))
+            (!projectName ||
+                entry.buildSettings.INFOPLIST_FILE.includes(`${projectName}/${projectName}-Info.plist`))
         );
     });
     const plist_file = path.join(project_dir, plist_file_entry.buildSettings.INFOPLIST_FILE.replace(/^"(.*)"$/g, '$1').replace(/\\&/g, '&'));
