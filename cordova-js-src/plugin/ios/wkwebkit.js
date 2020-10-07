70efc66d047c872c9d2d4a24bc4ab24c188ecce2
Niklas Merz <nmerz@gedys-intraware.de>
2020-10-07 16:20:35 +0200

    Move proxy to own function

--- cordova-js-src/plugin/ios/wkwebkit.js
+++ cordova-js-src/plugin/ios/wkwebkit.js
@@ -42,6 +42,18 @@ var WkWebKit = {
             return window.CDV_ASSETS_URL + '/_https_proxy_' + encodeURIComponent(path.replace('https://', ''));
         }
         return path;
+    },
+    convertProxyUrl: function (path) {
+        if (!path || !window.CDV_ASSETS_URL) {
+            return path;
+        }
+        if (path.startsWith('http://')) {
+            return window.CDV_ASSETS_URL + '/_http_proxy_' + encodeURIComponent(path.replace('http://', ''));
+        }
+        if (path.startsWith('https://')) {
+            return window.CDV_ASSETS_URL + '/_https_proxy_' + encodeURIComponent(path.replace('https://', ''));
+        }
+        return path;
     }
 };
 
