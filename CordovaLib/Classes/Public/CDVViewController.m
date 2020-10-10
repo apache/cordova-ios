cb20c9bba970136c98571cc6f1a4c97501c4aff7
Dimitri B <BendingBender@users.noreply.github.com>
2020-10-10 22:58:51 +0200

    Hide splash sceen immediately on .hide() (#1006)
    Make hiding of splash screen a race between `SplashScreenDelay` setting
    and navigator.splashscreen.hide().
    
    Related issue: https://github.com/apache/cordova-ios/issues/991

--- CordovaLib/Classes/Public/CDVViewController.m
+++ CordovaLib/Classes/Public/CDVViewController.m
@@ -767,7 +767,18 @@
     self.webView.hidden = NO;
 
     if ([self.settings cordovaBoolSettingForKey:@"AutoHideSplashScreen" defaultValue:YES]) {
-       [self showLaunchScreen:NO];
+        CGFloat splashScreenDelaySetting = [self.settings cordovaFloatSettingForKey:@"SplashScreenDelay" defaultValue:0];
+
+        if (splashScreenDelaySetting == 0) {
+            [self showLaunchScreen:NO];
+        } else {
+            // Divide by 1000 because config returns milliseconds and NSTimer takes seconds
+            CGFloat splashScreenDelay = splashScreenDelaySetting / 1000;
+
+            [NSTimer scheduledTimerWithTimeInterval:splashScreenDelay repeats:NO block:^(NSTimer * _Nonnull timer) {
+                [self showLaunchScreen:NO];
+            }];
+        }
     }
 }
 
@@ -776,22 +787,16 @@
  */
 - (void)showLaunchScreen:(BOOL)visible
 {
-    CGFloat splashScreenDelay = [self.settings cordovaFloatSettingForKey:@"SplashScreenDelay" defaultValue:0];
-
-    // AnimateWithDuration takes seconds but cordova documentation specifies milliseconds
     CGFloat fadeSplashScreenDuration = [self.settings cordovaFloatSettingForKey:@"FadeSplashScreenDuration" defaultValue:250];
 
     // Setting minimum value for fade to 0.25 seconds
     fadeSplashScreenDuration = fadeSplashScreenDuration < 250 ? 250 : fadeSplashScreenDuration;
 
-    // Divide by 1000 because config returns milliseconds and NSTimer takes seconds
-    CGFloat delayToFade = (MAX(splashScreenDelay, fadeSplashScreenDuration) - fadeSplashScreenDuration)/1000;
+    // AnimateWithDuration takes seconds but cordova documentation specifies milliseconds
     CGFloat fadeDuration = fadeSplashScreenDuration/1000;
 
-    [NSTimer scheduledTimerWithTimeInterval:delayToFade repeats:NO block:^(NSTimer * _Nonnull timer) {
-        [UIView animateWithDuration:fadeDuration animations:^{
-            [self.launchView setAlpha:(visible ? 1 : 0)];
-        }];
+    [UIView animateWithDuration:fadeDuration animations:^{
+        [self.launchView setAlpha:(visible ? 1 : 0)];
     }];
 }
 
