6636ceef7fb5639eff52b6c40def3677c7a387b8
Phillip Plum <p.plum@ww-consulting.net>
2022-06-22 16:39:21 +0200

    version 6.3.0

--- CordovaLib/Classes/Public/CDVAvailability.h
+++ CordovaLib/Classes/Public/CDVAvailability.h
@@ -78,6 +78,7 @@
 #define __CORDOVA_6_0_0 60000
 #define __CORDOVA_6_1_0 60100
 #define __CORDOVA_6_2_0 60200
+#define __CORDOVA_6_3_0 60300
 /* coho:next-version,insert-before */
 #define __CORDOVA_NA 99999      /* not available */
 
@@ -90,7 +91,7 @@
  */
 #ifndef CORDOVA_VERSION_MIN_REQUIRED
     /* coho:next-version-min-required,replace-after */
-    #define CORDOVA_VERSION_MIN_REQUIRED __CORDOVA_6_2_0
+    #define CORDOVA_VERSION_MIN_REQUIRED __CORDOVA_6_3_0
 #endif
 
 /*
--- bin/templates/scripts/cordova/Api.js
+++ bin/templates/scripts/cordova/Api.js
@@ -26,7 +26,7 @@
  *  This workflow would not have the `package.json` file.
  */
 // Coho updates this line
-const VERSION = '6.2.0';
+const VERSION = '6.3.0';
 
 const fs = require('fs-extra');
 const path = require('path');
