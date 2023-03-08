51f2317106b12f76ed92f8e2f9236339243dc9ef
KUDO Ken-ichi <kudo.k@wingarc.com>
2023-03-08 17:34:01 +0900

    iOSビルドが不安定なことへの回避策
    ref: https://github.com/apache/cordova-ios/issues/764#issuecomment-669317730

--- bin/templates/scripts/cordova/lib/projectFile.js
+++ bin/templates/scripts/cordova/lib/projectFile.js
@@ -39,8 +39,16 @@ function parseProjectFile (locations) {
     const xcodeproj = xcode.project(pbxPath);
     xcodeproj.parseSync();
 
+    const projectName = fs
+        .readdirSync(project_dir)
+        .find(d => d.includes(".xcworkspace"))
+        .replace(".xcworkspace", "");
     const xcBuildConfiguration = xcodeproj.pbxXCBuildConfigurationSection();
-    const plist_file_entry = _.find(xcBuildConfiguration, entry => entry.buildSettings && entry.buildSettings.INFOPLIST_FILE);
+    const plist_file_entry = _.find(xcBuildConfiguration, entry =>
+            entry.buildSettings &&
+            entry.buildSettings.INFOPLIST_FILE &&
+            entry.buildSettings.INFOPLIST_FILE.includes(projectName)
+    );
     const plist_file = path.join(project_dir, plist_file_entry.buildSettings.INFOPLIST_FILE.replace(/^"(.*)"$/g, '$1').replace(/\\&/g, '&'));
     const config_file = path.join(path.dirname(plist_file), 'config.xml');
 
