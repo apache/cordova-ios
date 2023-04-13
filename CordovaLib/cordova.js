827760ab82fbfe3ae9b1e5ed3e42ed9830f2bbed
Niklas Merz <niklasmerz@apache.org>
2023-04-13 11:56:44 +0200

    chore: manually update versions

--- CordovaLib/cordova.js
+++ CordovaLib/cordova.js
@@ -19,7 +19,7 @@
  under the License.
 */
 ;(function() {
-var PLATFORM_VERSION_BUILD_LABEL = '6.2.0';
+var PLATFORM_VERSION_BUILD_LABEL = '6.3.0';
 // file: src/scripts/require.js
 var require;
 var define;
