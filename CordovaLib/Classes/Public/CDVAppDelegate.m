b78600b032cde9a5458be7a9c02c4fa58773c67c
Darryl Pogue <darryl@dpogue.ca>
2024-05-29 02:05:40 -0700

    refactor: Remove outdated app init code from CDVAppDelegate
    The template's storyboard should handle the initialization of the view
    controller, rather than constructing it manually here.

--- CordovaLib/Classes/Public/CDVAppDelegate.m
+++ CordovaLib/Classes/Public/CDVAppDelegate.m
@@ -35,31 +35,8 @@
     return YES;
 }
 
-/**
- * This is main kick off after the app inits, the views and Settings are setup here. (preferred - iOS4 and up)
- */
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary<UIApplicationLaunchOptionsKey, id> *)launchOptions
 {
-    CGRect screenBounds = [[UIScreen mainScreen] bounds];
-
-    self.window = [[UIWindow alloc] initWithFrame:screenBounds];
-    self.window.autoresizesSubviews = YES;
-
-    // only set if not already set in subclass
-    if (self.viewController == nil) {
-        self.viewController = [[CDVViewController alloc] init];
-    }
-
-    // Set your app's start page by setting the <content src='foo.html' /> tag in config.xml.
-    // If necessary, uncomment the line below to override it.
-    // self.viewController.startPage = @"index.html";
-
-    // NOTE: To customize the view's frame size (which defaults to full screen), override
-    // [self.viewController viewWillAppear:] in your view controller.
-
-    self.window.rootViewController = self.viewController;
-    [self.window makeKeyAndVisible];
-
     return YES;
 }
 
