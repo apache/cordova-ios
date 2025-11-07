6aac75188493eb77417d9205729e976c7b0cd948
Erisu <erisu@apache.org>
2025-11-07 20:34:32 +0900

    feat!: use simctl to get list of simulator images

--- tests/spec/unit/lib/list-emulator-images.spec.js
+++ tests/spec/unit/lib/list-emulator-images.spec.js
@@ -1,38 +1,38 @@
-/*
-       Licensed to the Apache Software Foundation (ASF) under one
-       or more contributor license agreements.  See the NOTICE file
-       distributed with this work for additional information
-       regarding copyright ownership.  The ASF licenses this file
-       to you under the Apache License, Version 2.0 (the
-       "License"); you may not use this file except in compliance
-       with the License.  You may obtain a copy of the License at
+/**
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
 
-         http://www.apache.org/licenses/LICENSE-2.0
+        http://www.apache.org/licenses/LICENSE-2.0
 
-       Unless required by applicable law or agreed to in writing,
-       software distributed under the License is distributed on an
-       "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-       KIND, either express or implied.  See the License for the
-       specific language governing permissions and limitations
-       under the License.
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
 */
 
-// Requiring ios-sim below has some side effects, mainly,
+// Requiring simctl below has some side effects, mainly,
 // it ends up requiring the specific macOS environment bits that
 // allow for interacting with iOS Simulators. On Windows+Linux we are
 // bound to not-have-that.
 if (process.platform === 'darwin') {
     const list_emus = require('../../../../lib/listEmulatorImages');
-    const iossim = require('ios-sim');
+    const simctl = require('simctl');
 
-    describe('cordova/lib/listEmulatorImages', () => {
+    xdescribe('cordova/lib/listEmulatorImages', () => {
         describe('run method', () => {
             beforeEach(() => {
-                spyOn(iossim, 'getdevicetypes');
+                spyOn(simctl, 'list');
             });
-            it('should delegate to the ios-sim getdevicetypes method', () => {
+            it('should delegate to the simctl list method', () => {
                 list_emus.run();
-                expect(iossim.getdevicetypes).toHaveBeenCalled();
+                expect(simctl.list).toHaveBeenCalled();
             });
         });
     });
