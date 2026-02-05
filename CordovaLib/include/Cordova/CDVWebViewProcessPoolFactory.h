493f3ea3faae6d058c4c97163dffd1403e94216b
Manuel Beck <manuelbeck87@outlook.de>
2026-02-05 13:48:58 +0100

    fix(ios): Improve deprecation message for `CDVWebViewProcessPoolFactory`
    - Make clear, that the reason of deprecating `CDVWebViewProcessPoolFactory` is because of the deprecation of `WKProcessPool` by Apple since iOS 15

--- CordovaLib/include/Cordova/CDVWebViewProcessPoolFactory.h
+++ CordovaLib/include/Cordova/CDVWebViewProcessPoolFactory.h
@@ -26,7 +26,7 @@
     @Available(Cordova, introduced: "6.2.0", deprecated: "8.0.0")
  }
  */
-CDV_DEPRECATED(8.0.0, "WKProcessPool is deprecated since iOS 15.0: Creating and using multiple instances of WKProcessPool no longer has any effect.")
+CDV_DEPRECATED(8.0.0, "Apple deprecated  the use of WKProcessPool since iOS 15.0 with the reason: Creating and using multiple instances of WKProcessPool no longer has any effect.")
 @interface CDVWebViewProcessPoolFactory : NSObject
 @property (nonatomic, retain) WKProcessPool* sharedPool;
 
