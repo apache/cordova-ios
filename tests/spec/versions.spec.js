3b0bc90f679df74cb7f2429b472264004ac7d45f
Darryl Pogue <darryl@dpogue.ca>
2026-02-27 15:40:12 -0800

    refactor(versions): Refactor version code for test reliability

--- tests/spec/versions.spec.js
+++ tests/spec/versions.spec.js
@@ -17,10 +17,37 @@
  under the License.
  */
 
+const childProcess = require('node:child_process');
 const semver = require('semver');
 const versions = require('../../lib/versions');
 
+const xcodeSdksOutput = `
+DriverKit SDKs:
+        DriverKit 25.2                  -sdk driverkit25.2
+
+iOS SDKs:
+        iOS 26.2                        -sdk iphoneos26.2
+
+iOS Simulator SDKs:
+        Simulator - iOS 26.2            -sdk iphonesimulator26.2
+
+macOS SDKs:
+        macOS 15.7                      -sdk macosx15.7
+
+visionOS SDKs:
+        visionOS 26.2                   -sdk xros26.2
+
+visionOS Simulator SDKs:
+        Simulator - visionOS 26.2       -sdk xrsimulator26.2
+`;
+
 describe('versions', () => {
+    let spawnMock;
+
+    beforeEach(() => {
+        spawnMock = spyOn(childProcess, 'spawnSync');
+    });
+
     describe('compareVersions method', () => {
         it('calls semver.compare, given valid semver', () => {
             const testVersions = ['1.0.0', '1.1.0'];
@@ -51,51 +78,145 @@ describe('versions', () => {
                 versions.compareVersions('10.1-beta.1', '10.1')
             ).toBe(0);
         });
+
+        it('throws when given an invalid version', () => {
+            expect(() => versions.compareVersions('10.1', 'random string')).toThrowError(TypeError, 'Invalid Version: random string');
+        });
     });
 
-    // These tests can not run on windows.
-    if (process.platform === 'darwin') {
-        describe('get_apple_ios_version method', () => {
-            it('should have found ios version.', () => {
-                return versions.get_apple_ios_version().then(version => {
-                    expect(version).not.toBeUndefined();
-                });
-            }, 10000);
-        });
+    describe('get_apple_ios_version', () => {
+        it('should have found iOS version', () => {
+            spawnMock.and.returnValue({ status: 0, stdout: xcodeSdksOutput });
 
-        describe('get_apple_osx_version method', () => {
-            it('should have found osx version.', () => {
-                return versions.get_apple_osx_version().then(version => {
-                    expect(version).not.toBeUndefined();
-                });
+            return versions.get_apple_ios_version().then(version => {
+                expect(spawnMock).toHaveBeenCalledWith('xcodebuild', ['-showsdks'], jasmine.anything());
+                expect(version).toEqual('26.2');
             });
         });
 
-        describe('get_tool_version method', () => {
-            it('should not have found tool by name.', () => {
-                return versions.get_tool_version('unknown').then(
-                    () => fail('expected promise rejection'),
-                    error => expect(error.message).toContain('is not valid tool name')
-                );
-            }, 10000);
+        it('should fail if iOS SDK cannot be found', () => {
+            spawnMock.and.returnValue({ status: 0, stdout: '' });
 
-            it('should find xcodebuild version.', () => {
-                return versions.get_tool_version('xcodebuild').then((version) => {
-                    expect(version).not.toBe(undefined);
-                });
-            }, 10000);
-
-            it('should find ios-deploy version.', () => {
-                return versions.get_tool_version('ios-deploy').then((version) => {
-                    expect(version).not.toBe(undefined);
-                });
-            }, 10000);
-
-            it('should find pod version.', () => {
-                return versions.get_tool_version('pod').then((version) => {
-                    expect(version).not.toBe(undefined);
-                });
-            }, 20000); // The first invocation of `pod` can be quite slow
+            return versions.get_apple_ios_version().then(
+                _ => fail(),
+                err => expect(err.message).toMatch(/Could not determine iOS version/)
+            );
         });
-    }
+
+        it('should fail if the command return an error', () => {
+            spawnMock.and.returnValue({ error: new Error('Bad Command') });
+
+            return versions.get_apple_ios_version().then(
+                _ => fail(),
+                err => expect(err.message).toMatch(/Bad Command/)
+            );
+        });
+
+        it('should fail if the command return a non-zero status code', () => {
+            spawnMock.and.returnValue({ status: 1, stderr: 'Error message' });
+
+            return versions.get_apple_ios_version().then(
+                _ => fail(),
+                err => expect(err.message).toMatch(/Error message/)
+            );
+        });
+
+        it('should fail if xcodebuild cannot be found', () => {
+            spawnMock.and.throwError(new Error('ENOENT'));
+
+            return versions.get_apple_ios_version().then(
+                _ => fail(),
+                err => expect(err.message).toMatch(/ENOENT/)
+            );
+        });
+    });
+
+    describe('get_apple_osx_version', () => {
+        it('should have found macOS version', () => {
+            spawnMock.and.returnValue({ status: 0, stdout: xcodeSdksOutput });
+
+            return versions.get_apple_osx_version().then(version => {
+                expect(spawnMock).toHaveBeenCalledWith('xcodebuild', ['-showsdks'], jasmine.anything());
+                expect(version).toEqual('15.7');
+            });
+        });
+
+        it('should fail if macOS SDK cannot be found', () => {
+            spawnMock.and.returnValue({ status: 0, stdout: '' });
+
+            return versions.get_apple_osx_version().then(
+                _ => fail(),
+                err => expect(err.message).toMatch(/Could not determine macOS version/)
+            );
+        });
+    });
+
+    describe('get_tool_version', () => {
+        it('should not have found tool by name.', () => {
+            return versions.get_tool_version('unknown').then(
+                _ => fail('expected promise rejection'),
+                err => expect(err.message).toContain('is not valid tool name')
+            );
+        });
+
+        describe('xcodebuild', () => {
+            it('should find xcodebuild version', () => {
+                spawnMock.and.returnValue({ status: 0, stdout: 'Xcode 16.4' });
+
+                return versions.get_tool_version('xcodebuild').then((version) => {
+                    expect(spawnMock).toHaveBeenCalledWith('xcodebuild', ['-version'], jasmine.anything());
+                    expect(version).toEqual('16.4');
+                });
+            });
+
+            it('should fail if the version cannot be parsed', () => {
+                spawnMock.and.returnValue({ status: 0, stdout: 'Not a version' });
+
+                return versions.get_tool_version('xcodebuild').then(
+                    _ => fail(),
+                    err => expect(err.message).toMatch(/Could not determine Xcode version/)
+                );
+            });
+        });
+
+        describe('ios-deploy', () => {
+            it('should find ios-deploy version', () => {
+                spawnMock.and.returnValue({ status: 0, stdout: '1.12.2' });
+
+                return versions.get_tool_version('ios-deploy').then((version) => {
+                    expect(spawnMock).toHaveBeenCalledWith('ios-deploy', ['--version'], jasmine.anything());
+                    expect(version).toEqual('1.12.2');
+                });
+            });
+
+            it('should fail if ios-deploy cannot be found', () => {
+                spawnMock.and.throwError(new Error('ENOENT'));
+
+                return versions.get_tool_version('ios-deploy').then(
+                    _ => fail(),
+                    err => expect(err.message).toMatch(/ENOENT/)
+                );
+            });
+        });
+
+        describe('pod', () => {
+            it('should find ios-deploy version', () => {
+                spawnMock.and.returnValue({ status: 0, stdout: '1.16.2' });
+
+                return versions.get_tool_version('pod').then((version) => {
+                    expect(spawnMock).toHaveBeenCalledWith('pod', ['--version'], jasmine.anything());
+                    expect(version).toEqual('1.16.2');
+                });
+            });
+
+            it('should fail if pod cannot be found', () => {
+                spawnMock.and.throwError(new Error('ENOENT'));
+
+                return versions.get_tool_version('pod').then(
+                    _ => fail(),
+                    err => expect(err.message).toMatch(/ENOENT/)
+                );
+            });
+        });
+    });
 });
